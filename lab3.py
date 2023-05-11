79f7cfc5c682a48383b0b1be79e046103a0707fc
Scarecr0w9758 <132538423+Scarecr0w9758@users.noreply.github.com>
2023-05-11 09:46:34 +0500

    feat: Сделал лабу используя 4 функции (по условию)

--- lab3.py
+++ lab3.py
@@ -1,1 +1,38 @@
+# Реализовать программу, которая должна находить все
+# значения из заданного пользователем диапазона,
+# которые не встречаются
+# в качестве значений элементов массива p.
+# Разделить На 4 ФУНКЦИИ:
+# 1) вввод начала и конца диапазона 
+# 2) берём один элемент массива и он сравнивает себя в диапазоне
+# 3) запуск проги
+# 4) функция которая вызовет много функций№2
+
+def setInterval(start_interval, end_interval):
+    start_interval = int(input("Введите начало диапазона: "))
+    end_interval = int(input("Введите конец диапазона: "))
+    return (start_interval,end_interval)
+
+def checkOne(element, array,interval):
+    if element not in array and element in interval:
+        return True
+    else:
+        return False
+
+def getCheckedArray(array,interval):
+    returned_array=[]
+    for elem in array:
+        if (checkOne(elem,array,interval)):
+            returned_array.append(elem)
+    return returned_array
+
+def main(array,start,end):
+    return getCheckedArray(array,setInterval(start,end))
+    
+p_array=[]
+start=0
+end=0
+
+print("Числа, входящие в диапазон которые не присутствуют в массиве: ", main(p_array,start,end))
+
 
